966c6e87c540fabc1d31c36364f370067518fa19
LocTM <tranminhloc.anhk@gmail.com>
2024-04-01 14:15:23 +0700

    list trong state

--- demo3/src/App.js
+++ demo3/src/App.js
@@ -3,14 +3,16 @@ import './App.css';
 import Demo1 from "./Demo1";
 import Demo2 from "./Demo2";
 import Demo3 from "./Demo3";
+import Demo4 from "./Demo4";
 
 
 function App() {
   return (
       <>
-        <Demo1 x = {10}></Demo1>
-          <Demo2></Demo2>
-          <Demo3></Demo3>
+        {/*<Demo1 x = {10}></Demo1>*/}
+        {/*  <Demo2></Demo2>*/}
+        {/*  <Demo3></Demo3>*/}
+          <Demo4></Demo4>
       </>
 
   );
---
+++ demo3/src/Demo4.js
@@ -0,0 +1,37 @@
+import {Component} from "react";
+
+export default class Demo4 extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [
+                {
+                    name: "Tran Minh Loc",
+                    age: 30,
+                    class: "C1023H1"
+                },
+                {
+                    name: "Tran Tuan Anh",
+                    age: 30,
+                    class: "C1023H1"
+                },
+                {
+                    name: "Le Viet Hung",
+                    age: 20,
+                    class: "C1023H1"
+                }
+            ]
+
+
+        }
+    }
+    render() {
+        return(
+            <>
+                {this.state.list.map(item => (
+                    <h2>{item.name},{item.age},{item.class}</h2>
+                ))}
+            </>
+        )
+    }
+}
