de87b46c5950dcf5f02cadca20978682c47155cb
LocTM <tranminhloc.anhk@gmail.com>
2024-04-01 13:59:41 +0700

    isShow test 3

--- demo3/src/Demo2.js
+++ demo3/src/Demo2.js
@@ -4,22 +4,23 @@ import Inside from "./Inside";
 export default class Demo2 extends Component{
     constructor() {
         super();
-        this.state={
+        this.state = {
             isShow: true
         }
     }
     render() {
         return(
             <>
-                <Inside></Inside>
-                <button onClick={()=>
-                this.setState(state =>{
-                    return{
-                    isShow: !state.isShows
-                    }
-                })}>Nhấn vào đây</button>
+                {this.state.isShow && <Inside/>}
+                <button onClick={()=>{
+                    this.setState(state =>{
+                        return{
+                            isShow: !state.isShow
+                        }
+                    })
+                }}>
+                ClickMeAgain</button>
             </>
-
         )
     }
 }
